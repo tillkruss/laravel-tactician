d2f2f918cdc51ee1fe2c1d5c6ddd91c39b7c612c
Till Krüss <till@kruss.io>
2016-04-22 10:04:47 -0700

    Added `EventMiddleware` tests

---
+++ tests/Middleware/EventMiddlewareTest.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace TillKruss\LaravelTactician\Tests\Middleware;
+
+use Mockery;
+use Exception;
+use PHPUnit_Framework_TestCase;
+use Illuminate\Contracts\Events\Dispatcher;
+use TillKruss\LaravelTactician\Events\CommandFailed;
+use TillKruss\LaravelTactician\Events\CommandHandled;
+use TillKruss\LaravelTactician\Events\CommandReceived;
+use TillKruss\LaravelTactician\Middleware\EventMiddleware;
+use TillKruss\LaravelTactician\Tests\Fixtures\TestCommand;
+
+class EventMiddlewareTest extends PHPUnit_Framework_TestCase
+{
+    private $dispatcher;
+    private $middleware;
+
+    protected function setUp()
+    {
+        $this->dispatcher = Mockery::mock(Dispatcher::class);
+        $this->middleware = new EventMiddleware($this->dispatcher);
+    }
+
+    public function testSuccessEventsAreDispatched()
+    {
+        $this->dispatcher->shouldReceive('fire')->once()->with(Mockery::type(CommandReceived::class));
+        $this->dispatcher->shouldReceive('fire')->once()->with(Mockery::type(CommandHandled::class));
+
+        $this->middleware->execute(new TestCommand, function () {});
+    }
+
+    public function testFailureEventsAreDispatched()
+    {
+        $this->dispatcher->shouldReceive('fire')->once()->with(Mockery::type(CommandReceived::class));
+        $this->dispatcher->shouldReceive('fire')->once()->with(Mockery::type(CommandFailed::class));
+
+        $this->setExpectedException(Exception::class, 'Command Failed');
+
+        $next = function () use (&$executed) {
+            throw new Exception('Command Failed');
+        };
+
+        $this->middleware->execute(new TestCommand, $next);
+    }
+
+    public function testFailureEventExceptionsCanBeCaught()
+    {
+        $this->dispatcher->shouldReceive('fire')->with(Mockery::on(function ($event) {
+            $event->catchException();
+            return true;
+        }));
+
+        $next = function () use (&$executed) {
+            throw new Exception('Command Failed');
+        };
+
+        $this->middleware->execute(new TestCommand, $next);
+    }
+
+    public function testNextCallableIsInvoked()
+    {
+        $this->dispatcher->shouldIgnoreMissing();
+
+        $sentCommand = new TestCommand;
+        $receivedSameCommand = false;
+
+        $next = function ($receivedCommand) use (&$receivedSameCommand, $sentCommand) {
+            $receivedSameCommand = ($receivedCommand === $sentCommand);
+        };
+
+        $this->middleware->execute($sentCommand, $next);
+
+        $this->assertTrue($receivedSameCommand);
+    }
+}
